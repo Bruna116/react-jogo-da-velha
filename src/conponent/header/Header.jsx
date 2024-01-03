9dedc472c3eba81c368b75e841ea657ee3ddc9ba
Bruna116 <bruninha.charmosinha@hotmail.com>
2024-01-03 15:06:41 -0300

    Criado conponente de icone

--- src/conponent/header/Header.jsx
+++ src/conponent/header/Header.jsx
@@ -2,12 +2,16 @@ import styles from './Header.module.css'
 
 import Title from "../title/Title"
 import Subtitle from "../subtitle/SubTitle"
+import Icon from "../icon/Icon"
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Bruna Carvalho</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com" />
+      </div>
     </div>
   )
 }
