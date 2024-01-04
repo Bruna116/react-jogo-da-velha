fad7d4b6fe44815c5446b9273efa7044748c5503
Bruna116 <bruninha.charmosinha@hotmail.com>
2024-01-03 22:45:35 -0300

    Criado conponente game

---
+++ src/conponent/gameOpton/GameOption.jsx
@@ -0,0 +1,11 @@
+import styles from './GameOpton.module.css'
+
+function GameOption () {
+  return (
+    <div className={styles.gameOption}>
+      x
+    </div>
+  )
+}
+
+export default GameOption
